25fd93449e9f9549dd78e82395e219274bfa5316
hanigamini <hani.gamini1363@gmail.com>
2023-04-30 13:08:43 +0330

    First commit

---
+++ Enums.cs
@@ -0,0 +1,280 @@
+﻿using System.ComponentModel;
+
+namespace PishgamApi;
+
+public enum ApiStatusCode : byte
+{
+    [Description("موفقیت آمیز")]
+    Successful,
+    [Description("ارسال تکی انجام شده است")]
+    SingleSent,
+    [Description("ارسال نظیر به نظیر انجام شده است")]
+    P2PSent,
+    [Description("عدم شکست")]
+    NotFailure,
+    [Description("تعداد گیرندگان با تعداد پیامک همخوانی ندارد")]
+    MessageBodiesNotEqualToRecipientNumbers,
+    [Description("متن پیامک دارای کلمه(کلمات) فیلتر شده است")]
+    SmsTextIsFilter,
+    [Description("مجاز به ارسال لینک در متن پیام نمی باشید")]
+    LinkIsNotAllowed,
+    [Description("لیست گیرندگان خالی است")]
+    RecipientsIsEmpty,
+    [Description("سازمان پیدا نشد")]
+    OrganizationNotFound,
+    [Description("سازمان غیر فعال است")]
+    OrganizationIsInactive,
+    [Description("سازمان منقضی شده است")]
+    OrganizationIsExpired,
+    [Description("سازمان جاری یا یکی از سازمان های پدر غیرفعال است")]
+    OrganizationOrHierarchyIsInactive,
+    [Description("زمان ارسال خارج از بازه مجاز سازمان می باشد")]
+    OrganizationSendTimeNotValid,
+    [Description("ارسال کاربر غیر فعال است")]
+    UserSendInactive,
+    [Description("کاربر منقضی شده است")]
+    UserIsExpired,
+    [Description("اطلاعات کاربر پیدا نشد یا احراز هویت انجام نشده است")]
+    UserNotFound,
+    [Description("سازمان والد منقضی شده است")]
+    ParentOrganizationIsExpired,
+    [Description("شماره فرستنده معتبر یا فعال نیست، یا قابلیت ارسال از شما گرفته شده است")]
+    PrivateNumberNotValid,
+    [Description("شماره فرستنده غیرفعال است")]
+    PrivateNumberIsInactive,
+    [Description("شماره فرستنده منقضی شده است")]
+    PrivateNumberIsExpired,
+    [Description("سرویس ارسال در دسترس نمی باشد")]
+    SystemIsOutOfService,
+    [Description("خطا در ثبت در صف ارسال")]
+    ErrorInScheduledMessagesInsert,
+    [Description("لیست گیرندگان با توجه به لیست سیاه خالی است")]
+    ReceiverListBaseBlackListIsEmpty,
+    [Description("تعداد گیرندگان با توجه با بخش های پیامک بیشتر از 100 است")]
+    RecipientCountNotValid,
+    [Description("متن پیامک خالی است")]
+    MessageBodiesIsEmpty,
+    [Description("ناموفق")]
+    Failed,
+    [Description("تعداد گیرندگان بیشتر از 100 است")]
+    MaxRecipientsIs100,
+    [Description("خطا در ثبت در صف")]
+    ErrorInAddToRabbit,
+    [Description("شروع درخواست")]
+    BeginRequest,
+    [Description("پارامترها با پیام تعریف شده همخوانی ندارد")]
+    ParametersNotMatchMessage,
+    [Description("پارامترهای پیام نامعتبر")]
+    ParametersIsNull,
+    [Description("اطلاعات قالب پیامک احرازهویت یافت نشد")]
+    OtpNotFound,
+    [Description("ارسال قالب پیامک احرازهویت انجام شده است")]
+    OtpSent,
+    [Description("توکن خالی است")]
+    TokenIsNull,
+    [Description("توکن یافت نشد یا اشتباه وارد شده است یا تایید نشده است")]
+    TokenNotFound,
+    [Description("آی پی نامعتبر است")]
+    IpIsInvalid,
+    [Description("خطا در حذف بلک لیست ها")]
+    RemoveBlackListError,
+    [Description("تاریخ شروع یا پایان به اشتباه وارد شده است")]
+    DatesIsInvalid,
+    [Description("خطا در چک کردن استثناء ها ")]
+    CheckSendSmsExceptionError,
+    [Description("خطا در دریافت اطلاعات توکن")]
+    GetTokenInfoError,
+    [Description("خط امکان ارسال صوتی را ندارد")]
+    PrivateNumberNotSendVoice,
+    [Description("ارسال رمز یکبار مصرف صوتی انجام شد")]
+    VoiceOtpSent,
+    [Description("کد دلخواه باید حداقل 4 و حداکثر 8 رقم باشد")]
+    OptionalCodeLengthInvalid,
+    [Description("شماره همراه یا ثابت وارد نشده است")]
+    NumberIsEmpty,
+    [Description("شماره فرستنده وارد نشده است")]
+    SenderNumberIsEmpty,
+    [Description("خطا در کسر هزینه")]
+    DecreaseSendPriceError,
+    [Description("خطا در ثبت در صندوق ارسال و حذف از صف")]
+    ErrorInsertOutBoxMessageDeleteScheduledMessage,
+    [Description("عنوان وارد نشده است")]
+    TitleIsEmpty,
+    [Description("فایل وارد نشده است")]
+    FileIsEmpty,
+    [Description("فرمت فایل معتبر نیست")]
+    FileFormatNotValid,
+    [Description("آپلود فایل صوتی انجام شد")]
+    FileUploaded,
+    [Description("فایل صوتی تایید نشده")]
+    VoiceNotAccepted
+}
+
+public enum DeliveryStatus
+{
+    [Description("ارسال شده به اپراتور")]
+    SentToItc = 3,
+    [Description("رسیده به مخابرات")]
+    ReceivedByItc = 4,
+    [Description("لیست سیاه")]
+    BlackList = 14,
+    [Description("جدول لیست سیاه")]
+    BlackListTable = 30,
+    [Description("فرستنده معتبر نیست")]
+    InvalidSender = 26,
+    [Description("ارسال نشده")]
+    NotSent = 10,
+    [Description("رسیده به گوشی")]
+    SentAndReceivedbyPhone = 1,
+    [Description("نرسیده به گوشی")]
+    HaveNotReceivedToPhone = 2,
+    [Description("نرسیده به مخابرات")]
+    DidNotReceiveToItc = 5,
+    [Description("پیامک توسط سرور دریافت نشد")]
+    NotReceivebyServer = 6,
+    [Description("متن پیامک فیلتر میباشد")]
+    SmsIsFilter = 15,
+    [Description("ارسال شد")]
+    Sent = 9,
+    [Description("کمبود اعتبار")]
+    NotEnoughBalance = 18,
+    [Description("نام کاربری یا رمز عبور اشتباه است")]
+    InvalidUserOrPassword = 24,
+    [Description("موبایل معتبر نیست")]
+    InvalidMobile = 27,
+    [Description("ذخیره شده")]
+    Stored = 29,
+    [Description("متن پیام حاوی لینک است")]
+    SmsTextContainLink = 37,
+    [Description("شماره گیرنده از طرف اپراتور پشتیبانی نمی شود")]
+    NotSupportNumberFromOperator = 47,
+    [Description("خطا در ارسال")]
+    ErrorInSending = 7,
+    [Description("منتظر ارسال")]
+    WaitingForSend = 8,
+    [Description("در حال ارسال")]
+    IsSending = 12,
+    [Description("در انتظار تایید")]
+    WaitingForConfirmation = 17,
+    [Description("متن پیام خالی است")]
+    TextIsEmpty = 22,
+    [Description("تایید نشده")]
+    Rejected = 33,
+    [Description("شناسه پیامک یافت نشد")]
+    SmsIdNotFound = 34,
+    [Description("در صف ارسال")]
+    InQueue = 35,
+    [Description("متن پیام نامعتبر است")]
+    InvalidSmsText = 36,
+    [Description("بسته ارسالی بیشتر از 100")]
+    PacketGreaterThan100 = 38,
+    [Description("تعداد پیام و شماره همخوانی ندارد")]
+    MessagesCountAndNumberCountNotMatch = 39,
+    [Description("زمان ارسال نامتعبرست")]
+    InvalidSendTime = 40,
+    [Description("تعداد پیام و کدینگ همخوانی ندارد")]
+    MessageCountAndCodingCountNotMatch = 41,
+    [Description("اپراتور پشتیبانی نمیشود")]
+    OperatorNotSupport = 42,
+    [Description("تعرفه غیر معتبرست")]
+    InvalidGroupPrice = 43,
+    [Description("کدینگ پیام پشتیبانی نمیشود")]
+    CodingNotSupport = 44,
+    [Description("خط ارسال کننده غیرفعال")]
+    PrivateNumberInActive = 45,
+    [Description("خط ارسال کننده منقضی")]
+    PrivateNumberExpired = 46,
+    [Description("زمان ارسال غیر مجاز")]
+    IllegalSendTime = 48,
+    [Description("لغو از سوی کاربر(لغو11)")]
+    CanceledFromReceiver = 49,
+    [Description("کدینگ غیرمجاز")]
+    InvalidCoding = 50,
+    [Description("لغو از سوی اپراتور")]
+    CanceledFromOperator = 51,
+    [Description("منقضی از سوی اپراتور")]
+    ExpiredFromOperator = 52,
+    [Description("ردشده از سمت اپراتور")]
+    RejectFromOperator = 53,
+    [Description("نام کاربری یافت نشد")]
+    UserNameNotFound = 54,
+    [Description("کاربر منقضی")]
+    UserExpired = 55,
+    [Description("کاربر غیرفعال")]
+    InActiveUser = 56,
+    [Description("udh نامعتبر")]
+    UdhIsWrong = 57,
+    [Description("خطای سیستم از سوی اپراتور")]
+    OperatorSystemError = 58,
+    [Description("غیرمجاز")]
+    NotAccess = 59,
+    [Description("ردشده توسط مخابرات")]
+    RejectByItc = 60,
+    [Description("درخواست نامعتبر")]
+    InvalidRequest = 61,
+    [Description("خط ارسال کننده بلاک شده است")]
+    PrivateNumberIsBlocked = 62,
+    [Description("اپراتور در دسترس نیست")]
+    OperatorNotAvailable = 63,
+    [Description("نامشخص")]
+    Unknown = 0,
+}
+
+public enum SendVoiceOtpErrors
+{
+    WithoutError = 0,
+    [Description("شماره اشتباه میباشد")]
+    InvalidCellPhone = -2,
+    [Description("عدم موجودی کافی")]
+    NotEnoughBalance = -3,
+    [Description("طول کد غیرمجاز")]
+    InvalidCodeLength = -5,
+    [Description("زمان ارسال غیرمجاز میباشد")]
+    InvalidSendTime = -6,
+    [Description("امکان اتصال به سرور وجود ندارد")]
+    CannotConnectToServer = -20,
+    [Description("ثبت OTP غیرفعال میباشد")]
+    InActiveOtp = -25,
+    [Description("خطایی رخ داده است")]
+    ServerError = -500
+}
+
+public enum VoiceAuthorizationErrors
+{
+    WithoutError = 0,
+    [Description("نام کاربری یا رمز عبور اشتباه است")]
+    InvalidUserNameOrPassword = -1,
+    [Description("عدم احراز هویت")]
+    Forbidden = -10,
+    [Description("خطای ناشناخته")]
+    Unknown = -20,
+    [Description("عدم مجوز وب سرویس")]
+    NotAllowWebService = -100,
+    [Description("عدم احراز موبایل")]
+    MobileNotConfirm = -101,
+    [Description("کاربری منقضی شده")]
+    UserExpired = -102
+}
+
+public enum VoiceUploadErrors
+{
+    WithoutError = 0,
+    InvalidFormat = -10,
+    ErrorInConversion = -20,
+    ServerError = -500
+}
+
+/// <summary>
+/// وضعیت تایید صوت
+/// </summary>
+public enum VoiceStatus : byte
+{
+    [Description("در انتظار تایید")]
+    AwaitingConfirmation,
+    [Description("تایید شده")]
+    Accepted,
+    [Description("رد شده")]
+    Failed,
+    [Description("در انتظار تائید تلفنی")]
+    AwaitingPhoneConfirmation
+}
---
+++ Pishgam.cs
@@ -0,0 +1,273 @@
+﻿using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text.Json;
+using PishgamApi.Models.GetCredit;
+using PishgamApi.Models.GetDelivery;
+using PishgamApi.Models.GetMessage;
+using PishgamApi.Models.GetVoiceStatus;
+using PishgamApi.Models.SendOtp;
+using PishgamApi.Models.SendSms;
+using PishgamApi.Models.SendVoice;
+using PishgamApi.Models.SendVoiceOtp;
+using PishgamApi.Models.UploadVoice;
+using RestSharp;
+
+namespace PishgamApi;
+
+public class Pishgam
+{
+    private const string ApiAddress = "http://95.81.81.185:10680/";
+    private static readonly JsonSerializerOptions DefaultSerializerSettings = new() { PropertyNameCaseInsensitive = true };
+    /// <summary>
+    /// برای ارسال پیام استفاده می شود
+    /// </summary>
+    /// <param name="sendSmsRequest"></param>
+    /// <returns></returns>
+    public static async Task<SendSmsResponse> SendSms(SendSmsRequest sendSmsRequest)
+    {
+        var client = new RestClient();
+        var request = new RestRequest($"{ApiAddress}Messages/Send", Method.Post);
+        request.AddHeader("Authorization", sendSmsRequest.Token);
+        request.AddHeader("Content-Type", "application/json");
+        var body = new SendSmsRequest
+        {
+            MessageBodies = sendSmsRequest.MessageBodies,
+            RecipientNumbers = sendSmsRequest.RecipientNumbers,
+            SenderNumber = sendSmsRequest.SenderNumber
+        };
+        var jsonBody = JsonSerializer.Serialize(body);
+        request.AddParameter("application/json", jsonBody, ParameterType.RequestBody);
+        var response = await client.ExecuteAsync(request);
+        if (response.Content != null)
+            return JsonSerializer.Deserialize<SendSmsResponse>(response.Content, DefaultSerializerSettings) ??
+                   new SendSmsResponse { StatusCode = ApiStatusCode.Failed };
+
+        return new SendSmsResponse { StatusCode = ApiStatusCode.Failed };
+    }
+    
+    /// <summary>
+    /// میزان اعتبار کاربر را نمایش می دهد
+    /// </summary>
+    /// <param name="getCreditRequest"></param>
+    /// <returns></returns>
+    public static async Task<GetCreditResponse> GetCredit(GetCreditRequest getCreditRequest)
+    {
+        var client = new RestClient();
+        var request = new RestRequest($"{ApiAddress}Messages/GetCredit", Method.Get);
+        request.AddHeader("Authorization", getCreditRequest.Token);
+        request.AddParameter("text/plain", "", ParameterType.RequestBody);
+        var response = await client.ExecuteAsync(request);
+        if (response.Content != null)
+            return JsonSerializer.Deserialize<GetCreditResponse>(response.Content, DefaultSerializerSettings) ??
+                   new GetCreditResponse { StatusCode = ApiStatusCode.Failed };
+
+        return new GetCreditResponse { StatusCode = ApiStatusCode.Failed };
+    }
+    
+    /// <summary>
+    /// این متد در پاسخ  آرایه ای از وضعیت دریافت ها را به ترتیب شماره ها در آرایه ی گیرندگان داده شده نمایش می دهد. 
+    /// </summary>
+    /// <param name="getDeliveryRequest"></param>
+    /// <returns></returns>
+    public static async Task<GetDeliveryResponse> GetDelivery(GetDeliveryRequest getDeliveryRequest)
+    {
+        var client = new RestClient();
+        var request = new RestRequest($"{ApiAddress}Messages/GetDelivery", Method.Post);
+        request.AddHeader("Authorization", getDeliveryRequest.Token);
+        request.AddHeader("Content-Type", "application/json");
+        var body = new GetDeliveryRequest
+        {
+            MessageId = getDeliveryRequest.MessageId,
+            RecipientNumbers = getDeliveryRequest.RecipientNumbers
+        };
+        var jsonBody = JsonSerializer.Serialize(body);
+        request.AddParameter("application/json", jsonBody, ParameterType.RequestBody);
+        var response = await client.ExecuteAsync(request);
+        if (response.Content != null)
+            return JsonSerializer.Deserialize<GetDeliveryResponse>(response.Content, DefaultSerializerSettings) ??
+                   new GetDeliveryResponse { StatusCode = ApiStatusCode.Failed };
+
+        return new GetDeliveryResponse { StatusCode = ApiStatusCode.Failed };
+    }
+    
+    /// <summary>
+    /// این متد مربوط به دریافت پیام می باشد و حداکثر 100 پیام بر می گردد و بازه شروع و پایان را باید به صورت شمسی وارد نمایید.
+    /// </summary>
+    /// <param name="getMessageRequest"></param>
+    /// <returns></returns>
+    public static async Task<GetMessageResponse> GetMessage(GetMessageRequest getMessageRequest)
+    {
+        var client = new RestClient();
+        var request = new RestRequest($"{ApiAddress}Messages/GetMessage", Method.Post);
+        request.AddHeader("Authorization", getMessageRequest.Token);
+        request.AddHeader("Content-Type", "application/json");
+        var body = new GetMessageRequest
+        {
+            DateTimeFrom = getMessageRequest.DateTimeFrom,
+            DateTimeTo = getMessageRequest.DateTimeTo,
+            MarkAsRead = getMessageRequest.MarkAsRead,
+            PrivateNumber = getMessageRequest.PrivateNumber
+        };
+        var jsonBody = JsonSerializer.Serialize(body);
+        request.AddParameter("application/json", jsonBody, ParameterType.RequestBody);
+        var response = await client.ExecuteAsync(request);
+        if (response.Content != null)
+            return JsonSerializer.Deserialize<GetMessageResponse>(response.Content, DefaultSerializerSettings) ??
+                   new GetMessageResponse { StatusCode = ApiStatusCode.Failed };
+
+        return new GetMessageResponse { StatusCode = ApiStatusCode.Failed };
+    }
+
+    /// <summary>
+    /// از طریق این متد می توانید پیام صوتی با رمز یکبار مصرف را به صورت فوری به شماره(های) انتخابی خود ارسال نمایید
+    /// و با شماره وارد شده تماس گرفته میشود و رمز یکبار مصرف را به شما اعلام میکند .
+    /// اگر کد دلخواه  وارد شود کد کد دلخواه  خوانده می شود.
+    /// ولی اگر کد دلخواه وارد نشود کد 5 رقمی تولید شده خوانده می شود .
+    /// </summary>
+    /// <param name="sendVoiceOtpRequest"></param>
+    /// <returns></returns>
+    public static async Task<SendVoiceOtpResponse> SendVoiceOtp(SendVoiceOtpRequest sendVoiceOtpRequest)
+    {
+        var client = new RestClient();
+        var request = new RestRequest($"{ApiAddress}Messages/SendVoiceOtp", Method.Post);
+        request.AddHeader("Authorization", sendVoiceOtpRequest.Token);
+        request.AddHeader("Content-Type", "application/json");
+        var body = new SendVoiceOtpRequest
+        {
+            SenderNumber = sendVoiceOtpRequest.SenderNumber,
+            Number = sendVoiceOtpRequest.Number,
+            OptionalCode = sendVoiceOtpRequest.OptionalCode
+        };
+        var jsonBody = JsonSerializer.Serialize(body);
+        request.AddParameter("application/json", jsonBody, ParameterType.RequestBody);
+        var response = await client.ExecuteAsync(request);
+        if (response.Content != null)
+            return JsonSerializer.Deserialize<SendVoiceOtpResponse>(response.Content, DefaultSerializerSettings) ??
+                   new SendVoiceOtpResponse { StatusCode = ApiStatusCode.Failed };
+
+        return new SendVoiceOtpResponse { StatusCode = ApiStatusCode.Failed };
+    }
+
+    /// <summary>
+    /// میتوان به صورت byte صدای جدید آپلود کرد
+    /// </summary>
+    /// <param name="uploadVoiceMessageRequest"></param>
+    /// <returns></returns>
+    public static async Task<UploadVoiceMessageResponse> UploadVoiceMessage(UploadVoiceMessageRequest uploadVoiceMessageRequest)
+    {
+        try
+        {
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri(ApiAddress)
+            };
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", uploadVoiceMessageRequest.Token);
+
+            var uploadVoiceMessage = new UploadVoiceMessageRequest
+            {
+                SenderNumber = uploadVoiceMessageRequest.SenderNumber,
+                Title = uploadVoiceMessageRequest.Title,
+                File = uploadVoiceMessageRequest.File,
+                IsPersist = uploadVoiceMessageRequest.IsPersist,
+                CallFromCellPhone = uploadVoiceMessageRequest.CallFromCellPhone
+            };
+
+            var response = await client.PostAsJsonAsync("Messages/UploadVoiceMessage", uploadVoiceMessage);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<UploadVoiceMessageResponse>() ??
+                   new UploadVoiceMessageResponse { StatusCode = ApiStatusCode.Failed };
+
+        }
+        catch
+        {
+            return new UploadVoiceMessageResponse { StatusCode = ApiStatusCode.Failed };
+        }
+        
+    }
+    
+    /// <summary>
+    /// بررسی وضعیت فایل آپلود شده
+    /// </summary>
+    /// <param name="voiceStatusRequest"></param>
+    /// <returns></returns>
+    public static async Task<GetVoiceStatusResponse> GetVoiceStatus(GetVoiceStatusRequest voiceStatusRequest)
+    {
+        var client = new RestClient();
+        var request = new RestRequest($"{ApiAddress}Messages/GetVoiceStatus", Method.Post);
+        request.AddHeader("Authorization", voiceStatusRequest.Token);
+        request.AddHeader("Content-Type", "application/json");
+        var body = new GetVoiceStatusRequest
+        {
+            SenderNumber = voiceStatusRequest.SenderNumber,
+            MessageId = voiceStatusRequest.MessageId
+        };
+        var jsonBody = JsonSerializer.Serialize(body);
+        request.AddParameter("application/json", jsonBody, ParameterType.RequestBody);
+        var response = await client.ExecuteAsync(request);
+        if (response.Content != null)
+            return JsonSerializer.Deserialize<GetVoiceStatusResponse>(response.Content, DefaultSerializerSettings) ??
+                   new GetVoiceStatusResponse { StatusCode = ApiStatusCode.Failed };
+
+        return new GetVoiceStatusResponse { StatusCode = ApiStatusCode.Failed };
+    }
+
+    /// <summary>
+    /// ارسال پیامک صوتی
+    /// </summary>
+    /// <param name="sendVoiceRequest"></param>
+    /// <returns></returns>
+    public static async Task<SendVoiceResponse> SendVoice(SendVoiceRequest sendVoiceRequest)
+    {
+        var client = new RestClient();
+        var request = new RestRequest($"{ApiAddress}Messages/SendVoice", Method.Post);
+        request.AddHeader("Authorization", sendVoiceRequest.Token);
+        request.AddHeader("Content-Type", "application/json");
+        var body = new SendVoiceRequest
+        {
+            SenderNumber = sendVoiceRequest.SenderNumber,
+            RecipientNumbers = sendVoiceRequest.RecipientNumbers,
+            VoiceMessageId = sendVoiceRequest.VoiceMessageId,
+            MaxTryCount = sendVoiceRequest.MaxTryCount,
+            MinuteBetweenTries = sendVoiceRequest.MinuteBetweenTries
+        };
+        var jsonBody = JsonSerializer.Serialize(body);
+        request.AddParameter("application/json", jsonBody, ParameterType.RequestBody);
+        var response = await client.ExecuteAsync(request);
+        if (response.Content != null)
+            return JsonSerializer.Deserialize<SendVoiceResponse>(response.Content, DefaultSerializerSettings) ??
+                   new SendVoiceResponse { StatusCode = ApiStatusCode.Failed };
+
+        return new SendVoiceResponse { StatusCode = ApiStatusCode.Failed };
+    }
+
+    /// <summary>
+    /// ارسال پیامک احرازهویت
+    /// </summary>
+    /// <param name="sendOtpRequest"></param>
+    /// <returns></returns>
+    public static async Task<SendSmsResponse> SendOtp(SendOtpRequest sendOtpRequest)
+    {
+        var client = new RestClient();
+        var request = new RestRequest($"{ApiAddress}Messages/SendOtp", Method.Post);
+        request.AddHeader("Authorization", sendOtpRequest.Token);
+        request.AddHeader("Content-Type", "application/json");
+        var body = new SendOtpRequest
+        {
+            SenderNumber = sendOtpRequest.SenderNumber,
+            RecipientNumbers = sendOtpRequest.RecipientNumbers,
+            OtpId = sendOtpRequest.OtpId,
+            Parameters = sendOtpRequest.Parameters
+        };
+        var jsonBody = JsonSerializer.Serialize(body);
+        request.AddParameter("application/json", jsonBody, ParameterType.RequestBody);
+        var response = await client.ExecuteAsync(request);
+        if (response.Content != null)
+            return JsonSerializer.Deserialize<SendSmsResponse>(response.Content, DefaultSerializerSettings) ??
+                   new SendSmsResponse { StatusCode = ApiStatusCode.Failed };
+
+        return new SendSmsResponse { StatusCode = ApiStatusCode.Failed };
+    }
+
+}
